c197d1800694485fcde14991c83a1526a327b096
ntu-user <ntu-user@ntu-user>
2022-12-10 00:45:13 +0000

    first commit

---
+++ JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/App.java
@@ -0,0 +1,43 @@
+package com.mycompany.javafxapplication1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Stage secondaryStage = new Stage();
+        DB myObj = new DB();
+        myObj.log("-------- Simple Tutorial on how to make JDBC connection to SQLite DB ------------");
+        myObj.log("\n---------- Drop table ----------");
+        myObj.delTable(myObj.getTableName());
+        myObj.log("\n---------- Create table ----------");
+        myObj.createTable(myObj.getTableName());
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("primary.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 640, 480);
+            secondaryStage.setScene(scene);
+            secondaryStage.setTitle("Primary View");
+            secondaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
